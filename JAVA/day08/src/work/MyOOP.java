5dece5dd7100c8c287b223091c63f74bec59d158
DESKTOP-T5PIEOH\D <zzoo0720@naver.com>
2023-08-09 19:07:22 +0900

    230809 JAVA Practice001

---
+++ JAVA/day08/src/work/MyOOP.java
@@ -0,0 +1,26 @@
+package work;
+
+public class MyOOP {
+	public MyOOP(String[] args) {
+		Print p1 = new Print();
+		p1.delimiter = "----";
+		p1.A();
+		p1.A();
+		p1.B();
+		p1.B();
+		
+		Print p2 = new Print();
+		p2.delimiter = "***";
+		p2.A();
+		p2.A();
+		p2.B();
+		p2.B();
+		
+		p1.A();
+		p2.B();
+		p1.A();
+		p2.B();
+		
+	}
+
+}
---
+++ JAVA/day08/src/work/Print.java
@@ -0,0 +1,15 @@
+package work;
+
+class Print{
+	public String delimiter = "";
+	public void A() {
+		System.out.println("deliiter");
+		System.out.println("A");
+		System.out.println("A");
+	}
+	public void B() {
+		System.out.println("deliiter");
+		System.out.println("B");
+		System.out.println("B");
+	}
+}
